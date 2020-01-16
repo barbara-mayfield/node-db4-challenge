81ad4e5fc41cb24b157b4c454872e94ca56465ea
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-16 16:54:16 -0600

    Working on recipe-model helper functions and making api calls on getShoppingList and getInstructions

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,14 +1,25 @@
 const db = require("../data/db-config")
 
 function getRecipes() {
-    // SELECT * from recipes as r
-    // JOIN ingredients AS i ON i.id = r.id
-    // JOIN steps AS s ON s.id = r.id;
-
     return db("recipes as r")
         .select("*")
 } 
 
+function getShoppingList(id) {
+    // SELECT * from recipes_ingredients as ri
+    // JOIN ingredients AS i ON i.id = ri.ingredient_id;
+    return db("recipes_ingredients as ri")
+      .join("ingredients as i", "i.id", "i.quantity", "i.unit_measurement")
+      .select("i.ingredient", "i.quantity","i.unit_measurement")
+      .where("ri.ingredient_id", id);
+};
+
+function getInstructions(id) {
+    return db("instructions")
+}
+
 module.exports = {
     getRecipes,
+    getShoppingList,
+    getInstructions
 }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -13,4 +13,26 @@ router.get("/", async(req, res, next) => {
     }
 })
 
+router.get(":id/ingredients", async(req, res, next) => {
+    try {
+        const { id } = req.params
+        const ingredients = await recipeModel.getShoppingList(id)
+
+        res.json(ingredients)
+    } catch {
+        next(err)
+    }
+})
+
+router.get(":id/instructions", async(req, res, next) => {
+    try {
+        const { id } = req.params
+        const instructions = await recipeModel.getInstructions(id)
+
+        resizeBy.json(instructions)
+    } catch {
+        next(err)
+    }
+})
+
 module.exports = router;
