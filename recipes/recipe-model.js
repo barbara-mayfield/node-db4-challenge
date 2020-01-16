4482250e2b53aca565812314b0e70f7af31d849e
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-16 15:24:10 -0600

    working on migration files, getting error

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,13 +1,15 @@
 const db = require("../data/db-config")
 
-function getRecipes() {
+function getRecipes(id) {
     // SELECT r.name, r.author, i.ingredient
     // FROM recipes AS r
     // JOIN ingredients as i
     // WHERE i.id = r.id;
 
     return db("recipes as r")
-        .select("r.name", "r.author")
+        .join("ingredients as i", "i.ingredient", "i.id")
+        .where({ id })
+        .select("r.name", "r.author", "i.ingredient")
 } 
 
 module.exports = {
